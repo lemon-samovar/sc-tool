a095683000a457f39d5c04286d1f88c71759f807
lemon-samovar <lemon323323@yandex.ru>
2026-02-17 21:41:21 +0300

    fix: incorrect string slicing

--- src/Container.ts
+++ src/Container.ts
@@ -20,44 +20,42 @@ export class Container {
     
     constructor(data: ContainerData) {
         if (typeof data !== "string" && (data.format === "short")) {
-            this.ownerCode = data.containerNumber.slice(0, 5);
-            this.serialNumber = data.containerNumber.slice(5, 11);
-            this.checkDigit = data.containerNumber[11] ?? this.calculateCheckDigit();
-            this.typeCode = data.typeCode ?? '';
+            const fmtContNum = data.containerNumber.trim().toUpperCase();
+            this.ownerCode = fmtContNum.slice(0, 4);
+            this.serialNumber = fmtContNum.slice(4, 10);
+            this.checkDigit = fmtContNum[10] ?? this.calculateCheckDigit();
+            this.typeCode = data.typeCode?.trim().toUpperCase() ?? '';
         } else if (typeof data === "string") {
-            this.ownerCode = data.slice(0, 5);
-            this.serialNumber = data.slice(5, 11);
-            this.checkDigit = data[11] ?? this.calculateCheckDigit();
+            const fmtContNum = data.trim().toUpperCase();
+            this.ownerCode = fmtContNum.slice(0, 4);
+            this.serialNumber = fmtContNum.slice(4, 10);
+            this.checkDigit = fmtContNum[10] ?? this.calculateCheckDigit();
             this.typeCode = '';
         } else {
-            this.ownerCode = data.ownerCode;
+            this.ownerCode = data.ownerCode.trim().toUpperCase();
             this.serialNumber = data.serialNumber;
             this.checkDigit = data.checkDigit ?? this.calculateCheckDigit();
-            this.typeCode = data.typeCode ?? "";
+            this.typeCode = data.typeCode?.trim().toUpperCase() ?? '';
         }
     }    
 
-    calculateCheckDigit(): number | string {
-        if (this.ownerCode && this.serialNumber) {
-            const ownerCode = [...this.ownerCode];
-            let sum = 0;
-            ownerCode.forEach((letter, idx) => {
-                sum += lettersTable[letter] * 2**idx;
-            });
-            String(this.serialNumber).split("").forEach((element, idx) => {
-                sum += Number(element) * 2**(idx+4)
-            });
-            return sum % 11 == 10 ? 0 : sum % 11
-        } else {
-            return ''
-        }
+    calculateCheckDigit(): number {
+        const ownerCode = [...this.ownerCode];
+        let sum = 0;
+        ownerCode.forEach((letter, idx) => {
+            sum += lettersTable[letter] * 2**idx;
+        });
+        String(this.serialNumber).split("").forEach((element, idx) => {
+            sum += Number(element) * 2**(idx+4)
+        });
+        return sum % 11 == 10 ? 0 : sum % 11
     }
 
     fullContainerNumber(): string {
         const ownerCode = this.ownerCode ? this.ownerCode : 'XXXX';
         const serialNumber = this.serialNumber ? this.serialNumber : 'XXXXXX';
         const checkDigit = this.checkDigit ? this.checkDigit : 'X';
-        return (ownerCode + serialNumber + checkDigit).toUpperCase()
+        return ownerCode + serialNumber + checkDigit
     }
     
     typeInfo(): ContainerTypeData | null {
